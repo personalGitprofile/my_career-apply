96d7f30506c0c4391f88268f60d379fd3204c4c4
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-11-03 20:20:38 +0600

    router setup with children and outlat

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <h1 className="text-xl font-black text-emerald-900">MY Tailwind is working now!!!!!!</h1>
+      <h1 className="text-xl font-black text-emerald-900">MY Tailwind is working now!!!!!!</h1>
     <button className="btn btn-error text-white shadow">Accent</button>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [{
+      path:'/',
+      element:<Home></Home>
+    }
+  ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
