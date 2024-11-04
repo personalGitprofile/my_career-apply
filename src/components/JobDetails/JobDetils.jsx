62b8458a66d82d34c8f5a3f211f04e33b175e642
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-11-05 01:48:50 +0600

    created job detail page

--- src/components/JobDetails/JobDetils.jsx
+++ src/components/JobDetails/JobDetils.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 
 const JobDetils = () => {
@@ -11,7 +11,7 @@ const JobDetils = () => {
     // This is important to me!
     console.log(job)
     const { job_title, company_name, educational_requirements, experiences, job_description
-        ,job_responsibility,location,remote_or_onsite,salary,logo
+        ,job_responsibility,location,remote_or_onsite,salary,logo,contact_information
     }=job
     return (
         <div>
@@ -19,18 +19,51 @@ const JobDetils = () => {
              <div className="grid grid-cols-3 gap-7 mx-32 mb-7">
                 <div className="col-span-2 ">
                     <img className="mx-auto" src={ logo} alt="" />
-                <h1 className="text-center text-3xl py-16 font-medium text-blue-900">{job_title}</h1>
+                <h1 className="text-center text-3xl py-16 font-medium text-blue-900">{company_name}</h1>
                     <div>
                         <p className="pb-9"><span className="font-bold">Job Description:</span> {job_description }</p>
                         <p className="pb-9"><span className="font-bold">Job Responsibility:</span> {job_responsibility }</p>
                         <p className="pb-9"><span className="font-bold">Educational Requirements:</span> {educational_requirements }</p>
+                        <p className="pb-9"><span className="font-bold">Experiences::</span> {experiences }</p>
                 </div>
                 
 
             </div>
-            <div className="bg-slate-500">
-                <p>Right</p>
-            </div>
+                <div >
+                <div className="bg-blue-50 shadow-md rounded-md p-8">
+                    <p className="text-center text-xl font-bold">Details</p>
+                    <hr className="mt-3 border border-blue-200"/>
+                    <div className="mt-6">
+                        <div className="text-sm flex gap-2">
+                            <img src="https://i.ibb.co.com/bzL53m6/money.png" alt="" />
+                            <p><span className="font-bold">Salary :</span> {salary } (Per Month)</p>
+                        </div>
+                        <div className="text-sm flex gap-2 pt-2">
+                            <img src="https://i.ibb.co.com/Cv91RfJ/calendar.png" alt="" />
+                            <p><span className="font-bold">Job Title :</span> {job_title } </p>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <h1 className="text-center text-xl font-bold mt-8">Contact Information</h1>
+                        <hr className="mt-3 border border-blue-200" />
+                        
+                        <div className="text-sm flex gap-2 pt-2">
+                            <img src="https://i.ibb.co.com/58RQ4Mc/phone.png" alt="" />
+                            <p><span className="font-bold">Phone :</span> {contact_information.phone } </p>
+                        </div>
+                        <div className="text-sm flex gap-2 pt-2">
+                            <img src="https://i.ibb.co.com/8B50QXZ/email.png" alt="" />
+                            <p><span className="font-bold">Email:</span> {contact_information.email } </p>
+                        </div>
+                        <div className="text-sm flex gap-2 pt-2">
+                            <img src="https://i.ibb.co.com/54xxGf9/location2.png" alt="" />
+                            <p><span className="font-bold">Address :</span> {contact_information.address } </p>
+                        </div>
+                    </div>
+                    </div>
+                   <Link><p className="text-center mt-3"> <button className="btn bg-cyan-800 text-white px-12 ">Apply</button></p></Link>
+          </div>
         </div>
        </div>
             
