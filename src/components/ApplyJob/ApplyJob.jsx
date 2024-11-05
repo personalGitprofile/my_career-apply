443db52251c5786fcb220b111492643034c98169
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-11-05 21:59:06 +0600

    project edited

--- src/components/ApplyJob/ApplyJob.jsx
+++ src/components/ApplyJob/ApplyJob.jsx
@@ -4,6 +4,7 @@ const ApplyJob = ({apply}) => {
     return (
         <div className='border border-rose-900 p-5 rounded-md'>
             <h1>{apply.job_title}</h1>
+            <p className='text-sm text-gray-400'>Job Id: { apply.id}</p>
             
         </div>
     );
